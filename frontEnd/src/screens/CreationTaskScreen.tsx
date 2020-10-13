11029e62bc517f6058fe1c1ed39bbef1de89ee8b
thibaultspriet <thibaultspriet@hotmail.fr>
2020-10-13 14:32:10 +0200

    Resolve frontEnd submodule

---
+++ frontEnd/src/screens/CreationTaskScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const CreationTaskScreen = () => {
+    return (
+        <View>
+        <Text>Creation Task Screen < /Text>
+            < /View>
+    );
+};
+
+export default CreationTaskScreen;
