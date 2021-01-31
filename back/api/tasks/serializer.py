8df6d69db16caf062f17d6d511ef8e81f5fac0e7
thibaultspriet <thibaultspriet@hotmail.fr>
2021-01-31 19:23:20 +0100

    delete cate/diffi

--- back/api/tasks/serializer.py
+++ back/api/tasks/serializer.py
@@ -1,29 +1,9 @@
 from rest_framework import serializers
-from .models import Category, Difficulty, Task
-
-
-class DifficultySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Difficulty
-        fields = "__all__"
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = "__all__"
+from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(
-        many=False,
-        queryset=Category.objects.all()
-    )
-    difficulty = serializers.PrimaryKeyRelatedField(
-        many=False,
-        queryset=Difficulty.objects.all()
-    )
-
+    
     class Meta:
         model = Task
         fields = ('id', 'difficulty', 'category',
